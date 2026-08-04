6f7085fac56ce76fbf48e3fadda03ece91c0d669
Menezes0 <mendesmirian778@gmail.com>
2026-08-03 20:24:06 -0400

    aula02

---
+++ aula02/table.php
@@ -0,0 +1,44 @@
+<?php
+//iniciando a tabela 
+echo "<table border='1' width='300px' height='auto' style='text-align:center'>";
+
+//ordenando as linhas e colunas
+echo "<tr>";
+    echo "<td> Nome</td>";
+    echo "<td> Nota </td>";
+    echo "<td> Situação </td>";
+echo "</tr>";
+
+
+//ordenando a 2ª linha e colunas(1° aluno)
+echo "<tr>";
+    echo "<td> Mirian </td>";
+    echo "<td> 100 </td>";
+    echo "<td> Aprovado </td>";
+echo "</tr>";
+
+//ordenando a 3ª linha e colunas(2° aluno)
+echo "<tr>";
+    echo "<td> João </td>";
+    echo "<td> 50 </td>";
+    echo "<td> Reprovado </td>";
+echo "</tr>";
+
+//ordenando a 4ª linha e colunas(3° aluno)
+echo "<tr>";
+    echo "<td> Maria </td>";
+    echo "<td> 80 </td>";
+    echo "<td> Aprovado </td>";
+echo "</tr>";
+
+//ordenando a 5ª linha e colunas(4° aluno)
+echo "<tr>";
+    echo "<td> Pedro </td>";
+    echo "<td> 55 </td>";
+    echo "<td> Reprovado </td>";
+echo "</tr>";
+
+echo "</table>";    
+
+
+?>
